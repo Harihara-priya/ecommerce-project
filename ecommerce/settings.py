0a712e67be1913f6db7ecfe185863733fc022ba2
Harihara-priya <hannahpriya2801@gmail.com>
2024-10-15 12:53:01 +0530

    fixed error in setting

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store.apps.storeConfig',
+    'store.apps.StoreConfig',
 ]
 
 MIDDLEWARE = [
