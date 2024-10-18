b6ff90036ccf3341eda00369db7bd7cb222f953c
Harihara-priya <hannahpriya2801@gmail.com>
2024-10-19 00:31:29 +0530

    adding images,paths,views,shipping status,js file

--- store/models.py
+++ store/models.py
@@ -18,15 +18,15 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-@property
-def imageURL(self):
-	try:
-		url = self.image.url
-	except:
-		url = ''
-	return url
-    
-    
+    @property
+    def imageURL(self):
+       if self.image:
+          try:
+              return self.image.url
+          except AttributeError:
+              return '/static/images/default.png'  # Provide a default image URL
+       return '/static/images/default.png'
+       
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
@@ -36,12 +36,37 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
     
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if  i.product.digital == False:
+                shipping = True
+                return shipping
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+    
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+  
 class ShippingAddress(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
 	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
